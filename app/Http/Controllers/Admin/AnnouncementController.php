7675ad4d1f1ec2d25a10ef9aad1f0b436c254f4b
Faokushi <aspog0099@gmail.com>
2024-10-08 23:28:18 +0800

    asdasd

--- app/Http/Controllers/Admin/AnnouncementController.php
+++ app/Http/Controllers/Admin/AnnouncementController.php
@@ -10,7 +10,7 @@ class AnnouncementController extends Controller
 {
     public function index(){
         $data = Announcement::orderBy('id', 'desc')
-        ->all();
+        ->get();
         return response()->json(['announcement'=>$data]);
     }
     public function store(Request $request){
